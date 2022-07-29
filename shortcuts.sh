20f9c173e1da55a5b3ddcf98214d8f546863a1db
Daniel Wheeler <danielwheeler@25034-DanielWheeler.local>
2022-07-29 06:07:14 -0400

    Add script to copy personal shortcuts

--- shortcuts.sh
+++ shortcuts.sh
@@ -4,8 +4,9 @@ git config --global user.email "daniel.wheeler@customink.com"
 git config --global user.name "Daniel Wheeler"
 
 # git aliases
-alias status='git status'
-alias history='git log --pretty=format:\"%h %ad | %s%d [%an]\" --graph --date=short'
-alias checkout='git checkout'
-alias commit='git commit'
-alias branch='git branch'
+alias gst='git status'
+alias ghist='git log --pretty=format:\"%h %ad | %s%d [%an]\" --graph --date=short'
+alias gco='git checkout'
+alias gcm='git commit -m'
+alias gcmamend='git commit --amend --no-edit'
+alias gbr='git branch'
